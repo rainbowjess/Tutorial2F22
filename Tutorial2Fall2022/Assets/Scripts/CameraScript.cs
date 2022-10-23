c437fdda28ebaa7894bb4b4cd1b0d40d4f6edc94
rainbowjess <jessica.m.harrison11@gmail.com>
2022-10-23 15:10:34 -0400

    Challenge commit

--- Tutorial2Fall2022/Assets/Scripts/CameraScript.cs
+++ Tutorial2Fall2022/Assets/Scripts/CameraScript.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public GameObject target; 
+    public Transform target;
+
+    public float yOffset = 1f;
+    public float FollowSpeed = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,7 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
-
+        Vector3 newPos = new Vector3(target.position.x,target.position.y + yOffset,-10f);
+        transform.position = Vector3.Slerp(transform.position,newPos,FollowSpeed*Time.deltaTime);
     }
 }
--- Tutorial2Fall2022/Assets/Scripts/PlayerScript.cs
+++ Tutorial2Fall2022/Assets/Scripts/PlayerScript.cs
@@ -1,21 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
     public float speed;
-    public Text score;
-    private int scoreValue = 0;
+    public float jumpForce;
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI LivesText;
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
+    private int scoreValue;
+    private int livesValue;
+
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        scoreValue = 0;
+        rd2d = GetComponent<Rigidbody2D>();
+        livesValue = 3;
+        SetCountText();
+        winTextObject.SetActive(false);
+        SetCountText();
+        loseTextObject.SetActive(false);
     }
+
+    void SetCountText()
+    {
+        ScoreText.text = "Score: " + scoreValue.ToString();
+        LivesText.text = "Lives: " + livesValue.ToString();
+    }
+
+    void Update()
+    {
+        if (scoreValue == 5)
+        {
+            winTextObject.SetActive(true);
+            Destroy(gameObject);
+        }
+        
+        if (livesValue == 0)
+        {
+            loseTextObject.SetActive(true);
+            Destroy(gameObject);
+        }
+
+    }
+
+  
 
     // Update is called once per frame
     void FixedUpdate()
@@ -24,6 +64,8 @@ public class PlayerScript : MonoBehaviour
         float verMovement = Input.GetAxis("Vertical");
 
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -31,19 +73,26 @@ public class PlayerScript : MonoBehaviour
         if(collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            ScoreText.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+        }
+        if(collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+            livesValue = livesValue - 1;
+
+            SetCountText();
         }
 
     }
     
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if(collision.collider.tag == "Ground" && isOnGround)
         {
-            if (Input.GetKey(KeyCode.W))
+            if(Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
         }
 
